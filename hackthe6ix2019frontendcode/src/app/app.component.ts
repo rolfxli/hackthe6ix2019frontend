cbfdc27573979d7025bd757e2d2d8a9e78def21a
Rolf Li <rolf.x.li@gmail.com>
2019-08-23 22:24:56 -0400

    init

---
+++ hackthe6ix2019frontendcode/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'hackthe6ix2019frontendcode';
+}
