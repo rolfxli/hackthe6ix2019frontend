be41ef3cf401fa837ef3583ca2fb9dc6e1770b5a
Rolf Li <rolf.x.li@gmail.com>
2019-08-25 00:30:32 -0400

    changing urls

--- hackthe6ix2019frontendcode/src/app/urls.service.ts
+++ hackthe6ix2019frontendcode/src/app/urls.service.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 
 export class UrlsService {
 
-  uri = 'http://localhost:4000';
+  uri = 'http://ec2-3-17-165-213.us-east-2.compute.amazonaws.com:4000';
 
   constructor(private http: HttpClient) { }
 
